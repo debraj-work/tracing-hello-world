55fb19c036e8f1057c2832ae6a7917915dabdbcd
debraj-work <debraj.manna@nexla.com>
2025-04-28 15:59:19 +0530

    Fixes for RuleBasedRoutingSampler

--- otel-span-filter-extension/src/main/java/org/example/RuleBasedSamplerProvider.java
+++ otel-span-filter-extension/src/main/java/org/example/RuleBasedSamplerProvider.java
@@ -1,16 +1,19 @@
 package org.example;
 
 import io.opentelemetry.api.trace.SpanKind;
-import io.opentelemetry.contrib.sampler.RuleBasedRoutingSampler;
 import io.opentelemetry.sdk.autoconfigure.spi.ConfigProperties;
 import io.opentelemetry.sdk.autoconfigure.spi.traces.ConfigurableSamplerProvider;
 import io.opentelemetry.sdk.trace.samplers.Sampler;
 import io.opentelemetry.semconv.UrlAttributes;
 
+import java.util.logging.Logger;
+
 public class RuleBasedSamplerProvider implements ConfigurableSamplerProvider {
-    private static final String name = "rule-based-sampler";
+    private static final Logger logger = Logger.getLogger(RuleBasedSamplerProvider.class.getName());
+    private static final String name = "RuleBasedSamplerProvider";
     @Override
     public Sampler createSampler(ConfigProperties configProperties) {
+        logger.info("Creating Sampler for " + name);
         return RuleBasedRoutingSampler.builder(SpanKind.SERVER, Sampler.parentBased(Sampler.alwaysOn()))
                 .drop(UrlAttributes.URL_PATH, "^/metrics$")
                 .drop(UrlAttributes.URL_PATH, "^/health$")
